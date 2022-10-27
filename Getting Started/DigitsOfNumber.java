4b04181f43249749be557a122b45d6ee765a1171
unknown <parmeetnarang28@gmail.com>
2022-10-27 23:42:39 +0530

    -

---
+++ Getting Started/DigitsOfNumber.java
@@ -0,0 +1,17 @@
+/* import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int cnt = 0;
+    while (n != 0) {
+      n /= 10;
+      cnt++;
+    }
+    System.out.print(cnt);
+  }
+  
+} */
